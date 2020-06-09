6c947ecaa4a1e2668bc0264839228520dd474032
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-06-08 21:52:23 -0700

    Update Site

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+let lastScrollDown = 0
+let lastScrollUp = 0
+
 const handleWindowSize = () => {
     let jobDescription = document.getElementById("job-description")
 
@@ -12,4 +15,31 @@ const handleWindowSize = () => {
     return
 }
 
+const scrollFunction = () => {
+    if(Date.now() - lastScrollDown > 40) {
+      if (window.pageYOffset > 80) {
+        document.getElementById("brand").style.fontSize = "44px"
+        document.getElementById("nav").classList.remove("py-4")
+        document.getElementById("nav").classList.add("pt-4")
+        document.getElementById("job-description").style.cssText="display: none !important"
+      } 
+
+      lastScrollDown = Date.now()
+    }
+
+    if(Date.now() - lastScrollUp > 40) {
+      if (window.pageYOffset <= 80) {
+        document.getElementById("brand").style.cssText = "80px"
+        document.getElementById("nav").classList.add("py-4")
+        document.getElementById("nav").classList.remove("pt-4")
+        document.getElementById("job-description").style.cssText="display: block"
+      }
+
+      lastScrollUp = Date.now()
+    }
+
+    return
+}
+
 window.addEventListener("resize", handleWindowSize)
+window.addEventListener("scroll", scrollFunction)
